f9dde085a7e49d7d5a1c688a04ddcad59d9bd81f
Emmanuel Luis <ejluis2001@gmail.com>
2025-05-16 14:12:26 -0500

    Implemented Models & Tests

--- index.test.js
+++ index.test.js
@@ -1,3 +1,4 @@
+const { READCOMMITTED } = require('sequelize/lib/table-hints');
 const {sequelize} = require('./db')
 const {Restaurant, Menu} = require('./models/index')
 const {
@@ -5,6 +6,8 @@ const {
     seedMenu,
   } = require('./seedData');
 
+let resteraunts;
+let menus;
 describe('Restaurant and Menu Models', () => {
     /**
      * Runs the code prior to all tests
@@ -14,30 +17,33 @@ describe('Restaurant and Menu Models', () => {
         // by setting 'force:true' the tables are recreated each time the 
         // test suite is run
         await sequelize.sync({ force: true });
+
+        resteraunts = await Restaurant.bulkCreate(seedRestaurant);
+        menus = await Menu.bulkCreate(seedMenu);
     });
 
     test('can create a Restaurant', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        expect(resteraunts[0].name).toBe("AppleBees");
     });
 
     test('can create a Menu', async () => {
         // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        expect(menus[0].title).toEqual('Breakfast')
     });
 
     test('can find Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const foundRestaurant = await Restaurant.findByPk(1);
+        expect(foundRestaurant.name).toEqual('AppleBees');
     });
 
     test('can find Menus', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const foundMenu = await Menu.findByPk(1);
+        expect(foundMenu.title).toEqual('Breakfast');
     });
 
     test('can delete Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const deletedRestaurant = await resteraunts[0].destroy();
+        expect(deletedRestaurant.name).toEqual('AppleBees')
     });
 })
